cbf13c198b4550fc31ab9a3aa392c9f69538397d
yasmeenjalloud <107865694+yasmeenjalloud@users.noreply.github.com>
2023-02-24 22:40:32 +0200

    Add files via upload

---
+++ AI.py
@@ -0,0 +1,28 @@
+
+from teachable_machine import TeachableMachine
+import cv2 as cv
+
+cap = cv.VideoCapture(0)
+model = TeachableMachine(model_path="keras_model3.h5",
+                         labels_file_path="labels3.txt")
+
+image_path = "screenshot.jpg"
+
+while True:
+    _, img = cap.read()
+    cv.imwrite(image_path, img)
+
+    result = model.classify_image(image_path)
+
+    print("class_index", result["class_index"])
+
+    print("class_name:::", result["class_name"])
+
+    print("class_confidence:", result["class_confidence"])
+
+    print("predictions:", result["predictions"])
+
+    cv.imshow("Video Stream", img)
+
+    cv.waitKey(1)
+    
